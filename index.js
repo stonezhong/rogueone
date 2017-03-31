0854129df08436d16dfc4c03d90af23a6f7e54e2
Stone Zhong <stonezhong@hotmail.com>
2017-03-31 03:27:32 -0700

    update

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Scope = require('./lib/scope');
+const compiler = require('./lib/compiler');
+const rogueone = require('./lib/rogueone');
+
+module.exports = {
+    rogueone,
+    Scope,
+    compiler,
+};
